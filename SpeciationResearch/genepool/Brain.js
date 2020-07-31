30e4564424bef3cf000b7c4668e73454d6873a99
Jeffrey Ventrella <jeffreyventrella@gmail.com>
2020-07-31 16:52:48 -0700

    Add files via upload

---
+++ SpeciationResearch/genepool/Brain.js
@@ -0,0 +1,154 @@
+"use strict";
+
+//----------------------------------------
+// states
+//----------------------------------------
+const BRAIN_STATE_NULL                = -1;
+const BRAIN_STATE_RESTING             =  0;
+const BRAIN_STATE_LOOKING_FOR_MATE    =  1;
+const BRAIN_STATE_PURSUING_MATE       =  2;
+const BRAIN_STATE_LOOKING_FOR_FOOD    =  3;
+const BRAIN_STATE_PURSUING_FOOD       =  4;
+const BRAIN_STATE_LOOKING_FOR_PREY    =  5;
+const BRAIN_STATE_PURSUING_PREY       =  6;
+const BRAIN_STATE_FLEEING_PREDATOR    =  7;
+const NUM_BRAIN_STATES                =  8;
+
+//--------------------------------------------
+// perceiving
+//--------------------------------------------
+const BRAIN_SENSORY_UPDATE_PERIOD           = 50;
+const BRAIN_MAX_PERCEIVED_NEARBY_SWIMBOTS   = 20;
+
+//const BRAIN_FOCUS_TARGET_SHIFT_STRENGTH	    = 0.2;
+const BRAIN_FOCUS_TARGET_SHIFT_STRENGTH	    = 0.1;
+const BRAIN_FOCUS_TARGET_SHIFT_THRESHOLD    = 0.07;
+const BRAIN_WANDER_AMOUNT					= 0.2;
+//const BRAIN_WALL_BOUNCE_SHIFT_AMOUNT	    = 0.1;
+
+
+//----------------------------------------
+// Brain!
+//----------------------------------------
+function Brain()
+{
+	let _state                  = BRAIN_STATE_NULL;
+	let _energy                 = ZERO;
+	let _foundFoodBit           = false;
+	let _foundSwimbot           = false;
+	let _hungerThreshold        = ZERO;
+	let _attractionCriterion    = ATTRACTION_SIMILAR_COLOR;
+	
+	
+    //-----------------------------
+    this.initialize = function()
+    {
+	    _state = BRAIN_STATE_NULL;
+	    
+	    /*
+	    _energy                 = ZERO;
+	    _foundFoodBit           = false;
+	    _foundSwimbot           = false;
+	    _hungerThreshold        = ZERO;
+	    _attractionCriterion    = ATTRACTION_SIMILAR_COLOR;
+	    */
+    }
+
+    //-----------------------
+    this.update = function()
+    {   
+        //----------------------------------------------------------------------
+        // if low energy, look for food, otherwise, look for sex
+        //----------------------------------------------------------------------
+        if ( _energy < _hungerThreshold )
+        {
+            if (( _state != BRAIN_STATE_PURSUING_FOOD )
+            &&  ( _state != BRAIN_STATE_LOOKING_FOR_FOOD ))
+            {
+                _state = BRAIN_STATE_LOOKING_FOR_FOOD;
+            }       
+        }
+        else 
+        {
+            if (( _state != BRAIN_STATE_PURSUING_MATE )
+            &&  ( _state != BRAIN_STATE_LOOKING_FOR_MATE ))
+            {
+                _state = BRAIN_STATE_LOOKING_FOR_MATE;
+            }        
+        }
+    
+        //--------------------------------------------------------
+        //  looking for food
+        //--------------------------------------------------------
+        if ( _state == BRAIN_STATE_LOOKING_FOR_FOOD )
+        {
+            if ( _foundFoodBit )
+            {
+                _state = BRAIN_STATE_PURSUING_FOOD;
+            }
+        }	      
+        //--------------------------------------------------------
+        //  pursuing food
+        //--------------------------------------------------------
+        else if ( _state == BRAIN_STATE_PURSUING_FOOD )
+        {
+            if ( !_foundFoodBit )
+            {
+                _state = BRAIN_STATE_LOOKING_FOR_FOOD;
+            }
+        }
+        //--------------------------------------------------------
+        //  Looking for mate
+        //--------------------------------------------------------
+        else if ( _state == BRAIN_STATE_LOOKING_FOR_MATE )
+        {
+            if ( _foundSwimbot )
+            {
+                _state = BRAIN_STATE_PURSUING_MATE;
+            }
+        }
+        //--------------------------------------------------------
+        //  pursuing mate
+        //--------------------------------------------------------
+        else if ( _state == BRAIN_STATE_PURSUING_MATE )
+        {
+            if ( !_foundSwimbot )
+            {
+                _state = BRAIN_STATE_LOOKING_FOR_MATE;
+            }
+        }
+
+        //-----------------------------------------------------------------
+        //  check for bogus brain state 
+        //-----------------------------------------------------------------
+        assert( _state < NUM_BRAIN_STATES, "_state < NUM_BRAIN_STATES" );        
+        assert( _state > BRAIN_STATE_NULL, "_state > BRAIN_STATE_NULL" );
+    }
+
+    //-------------------------------------------------------------------
+    // setters
+    //-------------------------------------------------------------------
+    this.setEnergyLevel     = function(e) { _energy              = e; }   
+    this.setHungerThreshold = function(h) { _hungerThreshold     = h; }		
+    this.setFoundFoodBit    = function(f) { _foundFoodBit        = f; }
+    this.setFoundSwimbot    = function(f) { _foundSwimbot        = f; }		
+
+    //--------------------------------
+    this.setAttraction = function(a) 
+    {
+        //console.log( "setAttraction to " + a );
+        
+        _attractionCriterion = a; 
+
+        // setting _foundSwimbot to false, causes the swimbot to search for a new potential mate
+        _foundSwimbot = false;
+    }
+
+
+    //--------------------------------------------------------------------------
+    // getters
+    //--------------------------------------------------------------------------
+    this.getHungerThreshold     = function() { return _hungerThreshold;     }	
+    this.getAttractionCriterion = function() { return _attractionCriterion; }	
+    this.getState               = function() { return _state;               }
+}
---
+++ SpeciationResearch/genepool/SwimbotTypes.js
@@ -0,0 +1,184 @@
+"use strict";
+
+//-------------------------------------------
+//  time-related constants
+//-------------------------------------------
+const FULLY_GROWN_AGE    = 1000;
+const OLD_AGE            = 39000;
+const MAXIMUM_LIFESPAN   = 40000;
+const TIMER_DELTA_INCREASE_RATE = 0.02;
+const STARVING_TIMER_DELTA = 0.05;
+
+//----------------------------------------
+//  part indices
+//----------------------------------------
+const NULL_PART       = -1;
+const ROOT_PART       =  0;
+const MOUTH_INDEX	  =  0;
+const GENITAL_INDEX   =  1;
+const MIN_PARTS 	  =  2;
+const MAX_PARTS 	  = 16;
+
+//-------------------------------------------
+//  max population
+//-------------------------------------------
+const MAX_SWIMBOTS = 2000;
+
+//------------------------------------------
+// attraction
+//------------------------------------------
+const ATTRACTION_NULL               = -1;
+
+const ATTRACTION_COLORFUL           =  0;
+const ATTRACTION_BIG                =  1;
+const ATTRACTION_HYPER              =  2;
+const ATTRACTION_LONG               =  3;
+const ATTRACTION_STRAIGHT           =  4;
+
+const ATTRACTION_NO_COLOR           =  5;
+const ATTRACTION_SMALL              =  6;
+const ATTRACTION_STILL              =  7;
+const ATTRACTION_SHORT              =  8;
+const ATTRACTION_CROOKED            =  9;
+
+const ATTRACTION_SIMILAR_COLOR      = 10;
+const ATTRACTION_SIMILAR_SIZE       = 11;
+const ATTRACTION_SIMILAR_HYPER      = 12;
+const ATTRACTION_SIMILAR_LENGTH     = 13;
+const ATTRACTION_SIMILAR_STRAIGHT   = 14;
+
+const ATTRACTION_CLOSEST            = 15;
+const ATTRACTION_RANDOM             = 16;
+
+const NUM_ATTRACTIONS               = 17;
+
+//-----------------------------------
+//  metrics 
+//-----------------------------------
+const SWIMBOT_MIN_MOUTH_WIDTH   = 4.0;
+const SWIMBOT_MIN_MOUTH_LENGTH  = 8.0;
+const SWIMBOT_MOUTH_LENGTH      = 10.0;
+const SWIMBOT_GENITAL_LENGTH    = 10.0;
+const SWIMBOT_MOUTH_WIDTH       = 1.0;
+const SWIMBOT_GENITAL_WIDTH     = 1.0;
+const SWIMBOT_VIEW_RADIUS	    = 300.0;
+const SWIMBOT_EGG_RADIUS	    = 6.0;
+
+const SELECT_RADIUS_SCALAR      = 7.0;
+
+//const SWIMBOT_NUTRITION_ENERGY  = 10.0;
+
+
+//----------------------------------------
+//  LOD 
+//----------------------------------------
+const SWIMBOT_LEVEL_OF_DETAIL_LOW  = 0;
+const SWIMBOT_LEVEL_OF_DETAIL_HIGH = 1;
+
+
+//--------------------
+//  physics constants
+//--------------------
+//original
+//static const double SPIN_FORCE = 0.4;
+//static const double SPIN_DECAY = 0.95;
+
+//const SPIN_SCALAR     = 0.01;
+//const SPIN_DECAY      = 0.95;
+
+//before May 5:
+//const SPIN_SCALAR     = 5.0;
+//const SPIN_DECAY      = 0.9;
+
+//const SPIN_SCALAR     = 2.0;
+//const SPIN_SCALAR     = 1.0;
+//const SPIN_DECAY      = 0.95;
+
+const WALL_BOUNCE = 0.1;
+
+
+//---------------------------------------------------
+//  graphics 
+//---------------------------------------------------
+const SIZE_VIEW_SCALE = 0.03; //increase with view scale (a kind of LOD)
+
+
+//---------------------------------------------------
+//  energy 
+//---------------------------------------------------
+const ENERGY_USED_UP_SWIMMING				= 0.01;
+const STARVING								= 4.0;
+const CONTINUAL_ENERGY_DRAIN				= 0.0001;
+const MIN_CHILD_ENERGY_RATIO                = ZERO;
+const DEFAULT_CHILD_ENERGY_RATIO	        = ONE_HALF;
+const MAX_CHILD_ENERGY_RATIO                = ONE;
+const MIN_SWIMBOT_HUNGER_THRESHOLD          = ZERO;
+
+//const MAX_SWIMBOT_HUNGER_THRESHOLD          = 100;
+const MAX_SWIMBOT_HUNGER_THRESHOLD          = 200;
+
+const DEFAULT_SWIMBOT_HUNGER_THRESHOLD		= 50;
+//const DEFAULT_SWIMBOT_ATTRACTION            = ATTRACTION_SIMILAR_COLOR;
+
+const ENERGY_EFFICIENCY_MEASUREMENT_PERIOD  = 200;
+
+//--------------------
+//  Part 
+//--------------------
+function Part()
+{	
+	this.category			= 0;
+	this.position			= new Vector2D();   //dynamic
+	this.velocity			= new Vector2D();   //dynamic
+	this.axis 		        = new Vector2D();   //dynamic
+	this.previousMid 		= new Vector2D();   //dynamic
+	this.midPosition 		= new Vector2D();   //dynamic
+	this.perpendicular		= new Vector2D();   //dynamic
+	this.bendingAngle		= ZERO;             //dynamic
+	this.currentAngle		= ZERO;             //dynamic
+	this.parent 			= NULL_PART;
+	this.child              = NULL_PART; // only valid if it is the continuation of a single-category section
+	this.mass				= ZERO;
+	this.length				= ZERO;
+	this.width				= ZERO;
+    this.angle		        = ZERO;
+	this.frequency			= ZERO;
+	this.phase				= ZERO;
+	this.amp			    = ZERO;
+	this.turnAmp		    = ZERO;
+	this.turnPhase	        = ZERO;
+	this.momentFactor		= ZERO;
+	this.red				= ZERO;
+	this.green				= ZERO;
+	this.blue				= ZERO;
+    this.endCapSpline       = ZERO;     // how pointy the splined end-cap is for parts that terminate body sequence
+	this.branch             = false;    //set to true if this part branches off (not a continuation of a category)
+    this.splined		    = false;
+	this.numDecendents		= 0;
+	this.decendent			= new Array( MAX_PARTS );
+
+	for (let p=0; p<MAX_PARTS; p++)
+	{
+		this.decendent[p] = 0; 
+	}
+}
+
+//--------------------
+//  Phenotype 
+//--------------------
+function Phenotype()
+{	
+	this.numParts       = 0;
+	this.frequency      = ZERO;
+	this.parts          = new Array( MAX_PARTS ); 
+	this.sumPartLengths = ZERO;
+	this.mass           = ZERO;
+	this.preferredNutrition = 0;
+
+	for (let p=0; p<MAX_PARTS; p++)
+	{
+		this.parts[p] = new Part(); 
+	}
+}
+
+
