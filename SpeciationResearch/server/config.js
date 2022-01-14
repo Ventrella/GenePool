b4250c032d5a64059d9e2775227515979dd2ca92
Canton Becker <canton@canton-mbp.local>
2022-01-14 19:24:33 +0000

    Removing Speciation

--- SpeciationResearch/server/config.js
+++
@@ -1,9 +0,0 @@
-let config = {
-  apiKey: "AIzaSyDiJA3X71dojeKzhhFDsoRZFl71-4zHY40",
-  authDomain: "genepool-93eb8.firebaseapp.com",
-  databaseURL: "https://genepool-93eb8.firebaseio.com",
-  projectId: "genepool-93eb8",
-  storageBucket: "genepool-93eb8.appspot.com",
-  messagingSenderId: "723065741233",
-  appId: "1:723065741233:web:09f3ba75f5601d5f7da790"
-};
