23126ef2f9e92ff87bd91dee8cebaf6b5e548d0e
Jeffrey Ventrella <jeffreyventrella@gmail.com>
2022-01-14 15:14:13 -0800

    redid Canton's code with changes
    The pseudorandom code is currently broken. I have to ask Canton a question about usage.

--- GenePool/simulation/GenePool.js
+++ GenePool/simulation/GenePool.js
@@ -38,6 +38,44 @@ const CameraNavigationAction =
     OUT     : 5
 }
 
+var usingPseudoRandom = false;
+var pseudoRandomSeed = "defaultSeed";
+
+function gpRandom()
+{
+    if ( usingPseudoRandom )
+    {
+        /* ------------------  aleaPRNG 1.1  ------------------
+        Copyright (c) 2017-2020, W. "Mac" McMeans
+        LICENSE: BSD 3-Clause License
+        https://github.com/macmcmeans/aleaPRNG/blob/master/aleaPRNG-1.1.min.js
+        Use this to quickly generate random numbers with good statistical properties.
+
+        Return a 32-bit fraction in the range [0, 1] just like Math.random()
+        > gpRandom();  -->  0.30802189325913787
+ 
+        // advance the generator specified number of cycles
+        > myRandomNumbers.prng( 4 );
+
+        Want to 'rewind' back to the initial sequence?
+        > gpRandom.restart();
+        -------------------------------------------------------*/
+
+        /****************************************************************************
+         *** THREE DIFFERENT WAYS TO INITIALIZE THE RANDOM NUMBER GENERATOR:      ***
+         *** 1. gpRandom = aleaPRNG(); --> for an unpredictable start             ***
+         *** 2. gpRandom = aleaPRNG('some seed'); --> for a predictable start     ***
+         *** 3. function gpRandom() { return(Math.random()); } --> bypass PRNG    ***
+         *** ************************************************************************/
+
+        function aleaPRNG(){return function(n){"use strict";var r,t,e,o,a,u=new Uint32Array(3),i="";function c(n){var a=function(){var n=4022871197,r=function(r){r=r.toString();for(var t=0,e=r.length;t<e;t++){var o=.02519603282416938*(n+=r.charCodeAt(t));o-=n=o>>>0,n=(o*=n)>>>0,n+=4294967296*(o-=n)}return 2.3283064365386963e-10*(n>>>0)};return r.version="Mash 0.9",r}();r=a(" "),t=a(" "),e=a(" "),o=1;for(var u=0;u<n.length;u++)(r-=a(n[u]))<0&&(r+=1),(t-=a(n[u]))<0&&(t+=1),(e-=a(n[u]))<0&&(e+=1);i=a.version,a=null}function f(n){return parseInt(n,10)===n}var l=function(){var n=2091639*r+2.3283064365386963e-10*o;return r=t,t=e,e=n-(o=0|n)};return l.fract53=function(){return l()+1.1102230246251565e-16*(2097152*l()|0)},l.int32=function(){return 4294967296*l()},l.cycle=function(n){(n=void 0===n?1:+n)<1&&(n=1);for(var r=0;r<n;r++)l()},l.range=function(){var n,r;return 1===arguments.length?(n=0,r=arguments[0]):(n=arguments[0],r=arguments[1]),arguments[0]>arguments[1]&&(n=arguments[1],r=arguments[0]),f(n)&&f(r)?Math.floor(l()*(r-n+1))+n:l()*(r-n)+n},l.restart=function(){c(a)},l.seed=function(){c(Array.prototype.slice.call(arguments))},l.version=function(){return"aleaPRNG 1.1.0"},l.versions=function(){return"aleaPRNG 1.1.0, "+i},0===n.length&&(window.crypto.getRandomValues(u),n=[u[0],u[1],u[2]]),a=n,c(n),l}(Array.prototype.slice.call(arguments))}
+         
+        return aleaPRNG( pseudoRandomSeed );
+    }
+    
+    // default...
+    return Math.random();
+}
 
 
 // this needs to be the same as the corresponding value in Embryology.js !!!!
@@ -2328,15 +2366,12 @@ if ( globalTweakers.numFoodTypes === 2 )
         if ( _zoomingIn    ) {  _camera.zoomIn  (); }	
         if ( _zoomingOut   ) {  _camera.zoomOut (); }	
 	}
-	
-
 
 	//-----------------------------------------
 	this.setSimulationRunning = function(s)
 	{	
         _simulationRunning = s;
     }
-    
 
 	//-------------------------------
 	this.setRendering = function(r)
@@ -2344,14 +2379,18 @@ if ( globalTweakers.numFoodTypes === 2 )
         _rendering = r;
     }
     
-
 	//-------------------------------------------
 	this.setMillisecondsPerUpdate = function(m)
 	{	
         _millisecondsPerUpdate = m;
     }
-        
-
+    
+	//--------------------------------------------------
+	this.setMillisecondsPerUpdateToDefault = function()
+	{	
+        _millisecondsPerUpdate = DEFAULT_MILLISECONDS_PER_UPDATE;
+    }
+    
 	//-----------------------------------
 	this.toggleGoalOverlay = function()
 	{	
@@ -2394,6 +2433,18 @@ if ( globalTweakers.numFoodTypes === 2 )
 	this.setShowingViewTrackingMode = function(s)
 	{
         _showViewTrackingMode = s;		
+	}
+
+	//---------------------------------------------
+	this.setUsingPseudoRandom = function( using )
+	{
+	    usingPseudoRandom = using;
+	}
+	
+	//------------------------------------------
+	this.setPseudoRandomSeed = function( seed )
+	{
+	    pseudoRandomSeed = seed;
 	}
 
 	//-------------------------
